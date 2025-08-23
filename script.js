037ed0a49fc78350718621d74057eaeffb8a198c
arvndlr <arvin.dlrys@gmail.com>
2025-08-23 17:27:19 +0800

    move the icons to the footer

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const dropdownMenu = document.querySelector(".dropdown_menu");
-
-  menuToggle.addEventListener("click", function () {
-    dropdownMenu.classList.toggle("active");
-  });
-  
-});
